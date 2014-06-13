40de23c5728840e6a10fda45d5b3c409a928b5ef
freeteam31 <laurent.dunis@berger-levrault.fr>
2014-06-13 14:10:56 +0200

    Mise à jour de la classe User

--- FisrtIRCStarterWithUI/src/com/cfranc/irc/server/User.java
+++ FisrtIRCStarterWithUI/src/com/cfranc/irc/server/User.java
@@ -6,7 +6,28 @@ public class User {
 	
 	private String login;
 	private String pwd;
+	private String nom;
+	private String prenom;
+	private String cheminAvatar;
 	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getCheminAvatar() {
+		return cheminAvatar;
+	}
+	public void setCheminAvatar(String cheminAvatar) {
+		this.cheminAvatar = cheminAvatar;
+	}
 	public String getLogin() {
 		return login;
 	}
